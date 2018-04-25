84ae8a523b4acd7a00eb331f52581f30d0334526
ptcloutier <cloutier.perrin@gmail.com>
2018-04-25 00:50:38 -0400

    initial commit, OpenAL orbit

---
+++ CA9-OpenALOrbit/CA9-OpenALOrbit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CA9-OpenALOrbit
+//
+//  Created by Perrin Cloutier on 4/25/18.
+//  Copyright © 2018 Perrin Cloutier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
